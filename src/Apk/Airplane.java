8ac8db2a9134e3dbebd8f83019d8f253011f43b0
Martin1997 <slovakia99marty@gmail.com>
2019-10-19 23:48:25 +0200

    vytvorenie triedy lietadlo, a pridanie DateTimePickeru

--- src/Apk/Airplane.java
+++ src/Apk/Airplane.java
@@ -1,4 +1,58 @@
 package Apk;
 
+import java.time.LocalDateTime;
+
 public class Airplane {
+
+    private final String type; // nazov vyrobcu a oznacenie typu
+    private final String code; // jedinecny identifikator
+    private final int minRunwayLength;
+    private LocalDateTime arrival; // cas a datum priletu
+    private LocalDateTime runwayRequest; // cas a datum poziadania o priletovu drahu
+    private LocalDateTime departure; // cas a datum odletu
+    private int priority;
+
+    public Airplane(String type, String code, int minRunwayLength, LocalDateTime arrival, int priority) {
+        this.type = type;
+        this.code = code;
+        this.minRunwayLength = minRunwayLength;
+        this.arrival = arrival;
+        this.priority = priority;
+    }
+
+    public void setRunwayRequest(LocalDateTime runwayRequest) {
+        this.runwayRequest = runwayRequest;
+    }
+
+    public void setDeparture(LocalDateTime departure) {
+        this.departure = departure;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getMinRunwayLength() {
+        return minRunwayLength;
+    }
+
+    public LocalDateTime getArrival() {
+        return arrival;
+    }
+
+    public LocalDateTime getRunwayRequest() {
+        return runwayRequest;
+    }
+
+    public LocalDateTime getDeparture() {
+        return departure;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
